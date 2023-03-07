69e4bef74f89e7058446c461415d79869b2cf032
jerryfeng2003 <jerryfeng2003@gmail.com>
2023-03-07 17:34:50 +0800

    add

---
+++ DISK_C/COTTON/SOURCE/HELP.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<graphics.h>
+#include<dos.h>
+#include<bios.h>
+#include "TOTAL.H"
+
+void draw_help01()
+{
+    int i;
+    cleardevice();
+    setbkcolor(WHITE);
+	puthz(240,30,"°ïÖú¼°ËµÃ÷",32,32,BLUE);
+
+    mouseinit();
+	for(i=0;i<1000;i++)
+	{
+		newmouse(&MouseX,&MouseY,&press);
+		delay(5);
+	}
+	closegraph();
+}
